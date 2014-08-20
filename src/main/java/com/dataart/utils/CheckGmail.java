f3534efd247e703104faa4e202ab2d5df7faeddc
Oleg Shchegula <oleg.shchegula@dataart.com>
2014-08-20 12:11:57 +0300

    added resend email registration test

--- src/main/java/com/dataart/utils/CheckGmail.java
+++ src/main/java/com/dataart/utils/CheckGmail.java
@@ -36,13 +36,13 @@ public class CheckGmail {
 			}
 
 			for (Message message : messages) {
-				//System.out.println(message.getSubject().toString());
-				//System.out.println(InternetAddress.toString(message.getFrom()));
+				// System.out.println(message.getSubject().toString());
+				// System.out.println(InternetAddress.toString(message.getFrom()));
 				if (message.getSubject().toString()
 						.equals("ICPC Email confirmation")
 						&& InternetAddress.toString(message.getFrom())
 								.contains(Vars.ICPC_EMAIL)) {
-					
+
 					Pattern link_pattern = Pattern.compile("http://.*");
 
 					Object content = message.getContent();
@@ -65,6 +65,96 @@ public class CheckGmail {
 			System.out.println(e);
 		}
 		return confirmURL;
+	}
+
+	public static void deleteConfirmationMail(String email, String password) {
+
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.store.protocol", "imaps");
+		properties.setProperty("mail.imaps.partialfetch", "false");
+		try {
+			Session session = Session.getDefaultInstance(properties, null);
+			Store store = session.getStore("imaps");// create store instance
+			store.connect("imap.gmail.com", email, password);//
+			// store.connect(Vars.GMAIL_URL, email, password);
+			// System.out.println(store);
+			Folder inbox = store.getFolder("inbox");
+			inbox.open(Folder.READ_WRITE);// set access type of Inbox
+			Message messages[] = inbox.getMessages();// gets inbox messages
+			if (messages.length == 0) {
+				System.out.println("There is no message!");
+			}
+
+			for (Message message : messages) {
+				// System.out.println(message.getSubject().toString());
+				// System.out.println(InternetAddress.toString(message.getFrom()));
+				if (message.getSubject().toString()
+						.equals("ICPC Email confirmation")
+						&& InternetAddress.toString(message.getFrom())
+								.contains(Vars.ICPC_EMAIL)) {
+
+					message.setFlag(Flags.Flag.DELETED, true);
+				}
+			}
+			store.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+	
+	public static boolean checkConfirmationMail(String email, String password) {
+
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.store.protocol", "imaps");
+		properties.setProperty("mail.imaps.partialfetch", "false");
+		boolean flag = false;
+		try {
+			Session session = Session.getDefaultInstance(properties, null);
+			Store store = session.getStore("imaps");// create store instance
+			store.connect("imap.gmail.com", email, password);//
+			// store.connect(Vars.GMAIL_URL, email, password);
+			// System.out.println(store);
+			Folder inbox = store.getFolder("inbox");
+			inbox.open(Folder.READ_WRITE);// set access type of Inbox
+			Message messages[] = inbox.getMessages();// gets inbox messages
+			if (messages.length == 0) {
+				System.out.println("There is no message!");
+				return false;
+			}
+
+			for (Message message : messages) {
+				// System.out.println(message.getSubject().toString());
+				// System.out.println(InternetAddress.toString(message.getFrom()));
+				if (message.getSubject().toString()
+						.equals("ICPC Email confirmation")
+						&& InternetAddress.toString(message.getFrom())
+								.contains(Vars.ICPC_EMAIL)) {
+
+					Pattern link_pattern = Pattern.compile("http://.*");
+
+					Object content = message.getContent();
+					Multipart mp = (Multipart) content;
+
+					BodyPart bp = mp.getBodyPart(0);
+					Matcher matcher = link_pattern.matcher((String) bp
+							.getContent());
+
+					if (matcher.find()) {
+						System.out.println(matcher.group(0));
+						flag = true;
+
+					}
+					message.setFlag(Flags.Flag.DELETED, true);
+				}
+			}
+			store.close();
+			store.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return flag;
+
 	}
 
 }
--- src/test/java/com/dataart/jbehave/GeneralSteps.java
+++ src/test/java/com/dataart/jbehave/GeneralSteps.java
@@ -173,33 +173,44 @@ public class GeneralSteps {
 		StepFactory factory = new StepFactory(pages);
 		withTestDataFrom("/RegistrationData.csv").usingFactory(factory)
 				.run(userreg).enter_all_correct_credentials_form_file_source();
-		//userreg.user_should_see_the_Resend_email_button();
+		// userreg.user_should_see_the_Resend_email_button();
 	}
+
 	@When("enter not unique credentials")
-	public void userEnterNotUniqueCredentials(){
-		
+	public void userEnterNotUniqueCredentials() {
+
 		userreg.enter_not_unique_credentials();
 	}
+
 	@Then("user should see error message $message")
-	public void userShouldSeeErrorMessage(String message){
+	public void userShouldSeeErrorMessage(String message) {
 		userreg.user_should_see_DBerror_message(message);
 	}
+
 	@When("user enter all correct credentials")
-	public void userEnterAllCorrectCredentials(){
+	public void userEnterAllCorrectCredentials() {
 		userreg.user_enter_all_correct_credentials();
 	}
-	
+
 	@When("user check his emailbox $email $password and click on the confirmation link")
-	public void userCheckHisEmailbox(String email,String password){
-		userreg.user_check_his_emailbox_and_click_on_the_confirmation_link(email, password);
+	public void userCheckHisEmailbox(String email, String password) {
+		userreg.user_check_his_emailbox_and_click_on_the_confirmation_link(
+				email, password);
 	}
+
 	@Then("user should see the verified E-mail confirmation message $message")
-	public void userShouldSeeTheVerifiedEmailConfirmationMessage(String message){
+	public void userShouldSeeTheVerifiedEmailConfirmationMessage(String message) {
 		userreg.user_should_see_the_verified_Email_confirmation_message(message);
 	}
+
 	@When("user click on go to login page link")
-	public void userClickOnGoToLoginPageLink(){
+	public void userClickOnGoToLoginPageLink() {
 		userreg.user_click_on_go_to_login_page_link();
+	}
+
+	@Then("user click on Resend email button and check email")
+	public void user_click_Sign_up() {
+		userreg.user_click_on_Resend_email_button();
 	}
 
 }
